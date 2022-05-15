49895dbc8d5e6fa5997697d4d1998fcf2ee1e2c8
Alejandro Ramos <arhcoder@gmail.com>
2022-05-14 23:00:15 -0500

    Command Handler Template implemented
    The bot now can cut up a text command and executes it using different files. All the text commands logic is there, including if a message do not have a command, it does not process nothing about commands

---
+++ commands/commands.js
@@ -0,0 +1,4 @@
+module.exports.run = (client, message, args) =>
+{
+    message.channel.send("Comandos!");
+};
--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 "use strict";
 require("dotenv").config();
 const Discord = require("discord.js");
-const { MessageEmbed } = require("discord.js");
+const fs = require("fs");
+const variables = require("./variables");
+
+
+
 
 // Trying to launch the bot like a "client script"...
 console.log("Launching LZC Discord Bot...");
@@ -25,6 +29,74 @@ client.once("ready", () =>
     // Discord bot display for "Playing ---"...
 	client.user.setActivity("LZC 😎");
 });
+
+
+
+
+// Bot prefix to call commands...
+const prefix = "lzc";
+
+// Gets the commands collection to do a handler...
+client.commands = new Discord.Collection();
+const commands = fs.readdirSync("./commands").filter(file => file.endsWith(".js"));
+for(let file of commands)
+{
+    const commandName = file.split(".")[0];
+    const commandReference = require(`./commands/${commandName}`);
+    client.commands.set(commandName, commandReference);
+}
+
+
+
+
+// Actively listen for messages on the Discord server...
+client.on("messageCreate", message =>
+{
+    // Only if the message call a command, it searchs the command...
+    if (message.content.startsWith(prefix) || message.content === `<@${variables.botID}>` || message.content === `<@&${variables.botRoleID}>` || message.author.bot)
+    {
+        // Gets the command info to execute functions...
+        const args = message.content.slice(prefix.length + 1).split(/ +/);
+        var cmd = args.shift().toLowerCase();
+
+        // For the command "comandos" or "ayuda" or "help" or "commands"...
+        if(cmd === "comandos" || cmd === "ayuda" || cmd === "commands" || cmd === "help" ||
+        cmd === `<@${variables.botID}>` || message.content === `<@${variables.botID}>`
+        || cmd === `<@&${variables.botRoleID}>` || message.content === `<@&${variables.botRoleID}>`)
+            cmd = "commands";
+
+        // For the command "perfil" or "profile" or "stats" or "yo"...
+        else if(cmd === "perfil" || cmd === "profile" || cmd === "stats" || cmd === "yo")
+            cmd = "profile";
+
+        // For the command "logros" or "achievements"...
+        else if(cmd === "logros" || cmd === "achievements")
+            cmd = "achievements";
+
+        // For the command "insignias" or "roles" or "badges"...
+        else if(cmd === "insignias" || cmd === "roles" || cmd === "badges")
+            cmd = "badges";
+
+        // For the command "top" or "ranking"...
+        else if(cmd === "top" || cmd === "ranking")
+            cmd = "top";
+
+        // For the command "rank" or "nivel" or "level" or "rango"...
+        else if(cmd === "rank" || cmd === "nivel" || cmd === "level" || cmd === "rango")
+            cmd = "rank";
+        
+        // For the command "calendario" or "calendar"...
+        else if(cmd === "calendario" || cmd === "calendar")
+            cmd = "calendar";
+
+        // Executes the command if it exists...
+        const command = client.commands.get(cmd);
+        if(command) command.run(client, message, args);
+    }
+});
+
+
+
 
 // Login to Discord with your clien's token...
 client.login(TOKEN);
