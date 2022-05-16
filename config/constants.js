567d07616fae2aec0fe94125707bdf2903782658
Alejandro Ramos <arhcoder@gmail.com>
2022-05-16 02:00:51 -0500

    Separator icon changed
    This will be the definitive one

--- config/constants.js
+++ config/constants.js
@@ -3,7 +3,7 @@ module.exports =
     lzcolor: "#A0EC04",
     lzclogo: "https://yt3.ggpht.com/ytc/AKedOLTdJ8pbXJlwnwwGV_D4jAnPnlozISPPKufZunl1-Q=s900-c-k-c0x00ffffff-no-rj",
 
-    separator: "<:separator:975600160639946752>",
+    separator: "<:separator:975651401789218838>",
     leftArrow: "",
     rightArrow: ""
 }
