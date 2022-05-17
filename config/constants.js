158ff9b96056dd23f0486ffe691752a8acaa65ac
Alejandro Ramos <arhcoder@gmail.com>
2022-05-16 21:24:39 -0500

    Images url changed on the constants file
    Now they are hosted on the github repository, and the bot get them from it :3

--- config/constants.js
+++ config/constants.js
@@ -2,11 +2,11 @@ module.exports =
 {
     lzcolor: "#A0EC04",
     lzcmaincolor: "#2CDE08",
-    lzclogo: "https://yt3.ggpht.com/ytc/AKedOLTdJ8pbXJlwnwwGV_D4jAnPnlozISPPKufZunl1-Q=s900-c-k-c0x00ffffff-no-rj",
+    lzclogo: "https://raw.githubusercontent.com/arhcoder/LZC-Discord-Bot/master/images/lzc.png",
 
     lzcGuildID: "748216213645033503",
     
-    rankCardBackgroundPattern: "https://cdn.discordapp.com/attachments/975754156461666386/975915275159470150/lzcpattern.png",
+    rankCardBackgroundPattern: "https://raw.githubusercontent.com/arhcoder/LZC-Discord-Bot/master/images/pattern.png",
 
     separator: "<:separator:975651401789218838>",
     leftArrow: "",
